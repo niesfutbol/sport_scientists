112dbe8dbe118582ca7918adbe834cbaa006bb81
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-08-19 13:48:51 -0700

    🚧 Calculate mean speed by position

--- src/activity_1.R
+++ src/activity_1.R
@@ -1,3 +1,13 @@
 library(tidyverse)
 
 speed <- read_csv("/workdir/datos/actividad_1.csv")
+
+player_without_info <- speed |>
+  filter(`Total Duration` == 0) |>
+  pull(name)
+
+mean_speed_by_position <- speed |>
+  filter(`Total Duration` != 0) |>
+  group_by(position_name) |>
+  summarize(mean_speed = mean(`High speed distance (m)`)) |>
+  arrange(mean_speed)
