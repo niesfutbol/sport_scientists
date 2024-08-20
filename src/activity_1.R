c580acefb75ec0aaa5028f59bf32bdc43c303422
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-08-20 12:30:02 -0700

    🚧 Calculate `percentage_max_speed`, `high_intensity_acceleration` and `mean_speed`

--- src/activity_1.R
+++ src/activity_1.R
@@ -6,10 +6,32 @@ player_without_info <- speed |>
   filter(total_duration == 0) |>
   pull(name)
 
-mean_speed_by_position <- speed |>
+cleaned_speed <- speed |>
+  filter(total_duration != 0)
+
+mean_speed_by_position <- cleaned_speed |>
   filter(total_duration != 0) |>
   group_by(position_name) |>
   summarize(mean_speed = mean(high_speed_distance)) |>
   arrange(mean_speed)
 
 print(mean_speed_by_position)
+
+max_speed <- cleaned_speed |>
+  pull(`Maximum Velocity (km/h)`) |>
+  max()
+
+print(max_speed)
+
+max_speed <- cleaned_speed |>
+  filter(`Maximum Velocity (km/h)` > 40) |>
+  arrange(`Maximum Velocity (km/h)`) |>
+  print()
+
+new_speed <- cleaned_speed |>
+  mutate(
+    mean_speed = total_distance / total_duration,
+    percentage_max_speed = `Maximum Velocity (km/h)`*100/`Player Peak Velocity`,
+    high_intensity_acceleration = `Acceleration B2 Efforts (Gen 2)` + `Acceleration B3 Efforts (Gen 2)`
+  ) |>
+  select(c(mean_speed, percentage_max_speed, high_intensity_acceleration))
