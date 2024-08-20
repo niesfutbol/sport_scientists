d18b271b0cb7bb479755cb017b4328610ec9c312
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-08-20 11:56:54 -0700

    🚧 Use new field names

--- src/activity_1.R
+++ src/activity_1.R
@@ -1,13 +1,15 @@
 library(tidyverse)
 
-speed <- read_csv("/workdir/datos/actividad_1.csv")
+speed <- read_csv("/workdir/datos/actividad_1.csv", show_col_types = FALSE)
 
 player_without_info <- speed |>
-  filter(`Total Duration` == 0) |>
+  filter(total_duration == 0) |>
   pull(name)
 
 mean_speed_by_position <- speed |>
-  filter(`Total Duration` != 0) |>
+  filter(total_duration != 0) |>
   group_by(position_name) |>
-  summarize(mean_speed = mean(`High speed distance (m)`)) |>
+  summarize(mean_speed = mean(high_speed_distance)) |>
   arrange(mean_speed)
+
+print(mean_speed_by_position)
