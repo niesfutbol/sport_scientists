7346fcb8ecc39c9ec30338d38c434afa91f073b1
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-08-21 17:22:49 -0700

    🚧📉 Add two figures

--- src/activity_2.R
+++ src/activity_2.R
@@ -1,10 +1,31 @@
 library(tidyverse)
+library(ggplot2)
+
+crear_grafico <- function(df) {
+  ggplot(df, aes(x = countermovement_depth, y = concentric_mean_power_bm)) +
+    geom_point() +
+    labs(x = "Profundidad del contramovimiento",
+         y = "Potencia media concéntrica") +
+    ggtitle("Relación entre potencia y profundidad")
+}
+
+crear_grafico_serie_tiempo <- function(df) {
+  ggplot(df, aes(x = test_date, y = rsi_modified)) +
+    geom_line() +
+    labs(x = "Fecha", y = "RSI Modificado") +
+    ggtitle("Evolución del RSI Modificado")
+}
 
 # Paso 1
 jumps <- read_csv("/workdir/datos/actividad_2_fp.csv", show_col_types = FALSE)
 
+# Paso 2
+p <- jumps |>
+  crear_grafico()
+ggsave("results/step2.png")
 # Paso 3
-jumps |> filter(player_id == 4317)
+jumps |> filter(player_id == 4317) |> crear_grafico_serie_tiempo()
+ggsave("results/step3.png")
 
 # Paso 4
 jumps |> filter(test_date == 2021-02-26)
