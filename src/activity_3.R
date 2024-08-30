5665315db52fcf046d94d1f4c92a5d15716431f4
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-08-29 17:42:37 -0700

    🚧 Read activity 3 data

---
+++ src/activity_3.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+
+gps <- read_csv("/workdir/datos/Actividad 1 - data_gps_act1_c2.csv", show_col_types = FALSE)
+player_position <- read_csv("/workdir/datos/Actividad 1 - data_positions_act1_c2.csv", show_col_types = FALSE)
