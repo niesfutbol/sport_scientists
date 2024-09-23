78023e3d0b0caadc2555f481153e11bcf84ae64d
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-09-22 18:43:01 -0700

    🚧 Filter the player Pro 12

--- src/activity_3.R
+++ src/activity_3.R
@@ -2,3 +2,10 @@ library(tidyverse)
 
 gps <- read_csv("/workdir/datos/Actividad 1 - data_gps_act1_c2.csv", show_col_types = FALSE)
 player_position <- read_csv("/workdir/datos/Actividad 1 - data_positions_act1_c2.csv", show_col_types = FALSE)
+
+change_to_m_s <- function(speed_km_h) {
+  speed_m_s <- speed_km_h * (1000) * (1/3600)
+}
+gps_pro_12 <- gps |>
+  filter(player_display_name == "Pro 12") |>
+  mutate(speed_m_s = change_to_m_s(speed_km_h))
