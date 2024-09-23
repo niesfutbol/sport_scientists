e8bf565e3efdbde168a28cce58d2cdacb1ffcc09
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-09-22 20:58:43 -0700

    🚧 Calculate acceleration

--- src/activity_3.R
+++ src/activity_3.R
@@ -6,6 +6,11 @@ player_position <- read_csv("/workdir/datos/Actividad 1 - data_positions_act1_c2
 change_to_m_s <- function(speed_km_h) {
   speed_m_s <- speed_km_h * (1000) * (1/3600)
 }
+
 gps_pro_12 <- gps |>
   filter(player_display_name == "Pro 12") |>
-  mutate(speed_m_s = change_to_m_s(speed_km_h))
+  mutate(
+    speed_m_s = change_to_m_s(speed_km_h),
+    acceleration = (speed_m_s - lag(speed_m_s))/0.1,
+    acceleration_abs = abs(acceleration)
+  )
